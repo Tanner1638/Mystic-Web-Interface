5a044a1f031c3c3dcfbbcc74dd1517e460b0864e
Tanner1638 <tanner1638@gmail.com>
2021-01-25 22:18:03 -0600

    Add files via upload

---
+++ dashboard-client/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import { Switch, Route} from 'react-router-dom';
+import { LandingPage, MenuPage, DashboardPage} from './pages';
+
+function App() {
+  return (
+    <Switch>
+      <Route path="/" exact={true} component={ LandingPage } />
+      <Route path="/menu" exact={true} component={ MenuPage } />
+      <Route path="/dashboard" exact={true} component={ DashboardPage } />
+    </Switch>
+    
+  );
+}
+
+export default App;
