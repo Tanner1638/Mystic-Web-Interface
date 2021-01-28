04d48819b0796c2b67417e44052a670993699655
Tanner1638 <tanner1638@gmail.com>
2021-01-28 11:34:18 -0600

    Added guild initialize command

---
+++ simp-bot/src/commands/test/InitGuildCommand.js
@@ -0,0 +1,36 @@
+const BaseCommand = require('../../utils/structures/BaseCommand');
+const GuildConfig = require('../../database/schemas/GuildConfig');
+
+
+/**
+ * Used to initialize any guilds that the bot is in but is not listed in the database
+ * @version 1.0.0
+ */
+
+module.exports = class InitGuildCommand extends BaseCommand {
+  constructor() {
+    super('initGuild', 'test', []);
+  }
+  async run(client, message, args) {
+
+    try {
+      const guildConfig = await GuildConfig.create({
+        guildId: message.guild.id,
+      });
+  
+      console.log('Bot has joined the server. Saved to DB');
+      message.channel.send(`Complete!`);
+    } catch (err) {
+      message.channel.send(`An error has occured! Cannot complete action.`);
+      console.log(err);
+    }
+
+    
+
+  }
+}
+
+
+const initializeGuild = async (guildId) => {
+  
+}
