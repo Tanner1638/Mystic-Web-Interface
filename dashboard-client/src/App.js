c805a2131b7550b182a2d677e138dce981475859
Tanner1638 <tanner1638@gmail.com>
2021-01-28 12:57:48 -0600

    Pre Design Integration

--- dashboard-client/src/App.js
+++ dashboard-client/src/App.js
@@ -9,6 +9,11 @@ function App() {
       <Route path="/" exact={true} component={ LandingPage } />
       <Route path="/menu" exact={true} component={ MenuPage } />
       <Route path="/dashboard/:id" exact={true} component={ DashboardPage }/>
+      <Route
+        path="/dashboard/:guildId/general/muted"
+        exact={true}
+        component={DashboardPage}
+      />
     </Switch>
     
   );
