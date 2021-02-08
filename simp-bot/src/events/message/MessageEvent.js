7d3eb007aad38048791d5a22e3fbe08732fe4113
Mystic <tanner1638@gmail.com>
2021-02-08 16:53:53 -0600

    Add files via upload

--- simp-bot/src/events/message/MessageEvent.js
+++ simp-bot/src/events/message/MessageEvent.js
@@ -11,6 +11,13 @@ module.exports = class MessageEvent extends BaseEvent {
   
   async run(client, message) {
     if (message.author.bot) return;
+    if (message.guild.id == '807844741793316925'){
+      if(message.channel.id == '808169622994026497'){
+        globalSay(client, message.content);
+      }
+    }
+
+    //console.log(`Guild: ${message.guild.name}\nChannelName: ${message.channel.name}\nChannel ID: ${message.channel.id}\n${message.author.username}: ${message.content}\n`)
 
 
     
@@ -38,6 +45,43 @@ module.exports = class MessageEvent extends BaseEvent {
       if (command) {
         command.run(client, message, cmdArgs);
       }
+    }
+  }
+}
+
+function globalSay(client, message) {
+  // var targetGuild = '723441004002148374'; //Mystics OG Squad
+  // var targetChannel = '723441004002148377'; //General chat
+
+   var targetGuild = '733505271686627339'; //Taes server
+   var targetChannel = '733505271686627342'; //General chat
+
+   //var targetGuild = '520102286102495243'; //ZEO server
+   //var targetChannel = '520102286102495247'; //General chat
+
+   
+
+
+
+
+
+
+
+    // var targetGuild = '807844741793316925'; //Mystic Code
+    // var targetChannel = '807874255848341515'; //bot test
+  let guild = client.guilds.cache.get(targetGuild),
+    channel;
+
+
+  if (guild) {
+    channel = guild.channels.cache.get(targetChannel);
+    if (channel) {
+
+      let newMessage = message;
+      channel.send(newMessage);
+    }
+    else {
+      console.log("There's no channel with that ID.");
     }
   }
 }
