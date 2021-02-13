d27102fde8ba8f5ddb274da530d543fef15c34f1
bobrienn <bo9br@umsystem.edu>
2021-02-13 01:17:53 -0600

    updated command names

--- simp-bot/src/events/message/MessageEvent.js
+++ simp-bot/src/events/message/MessageEvent.js
@@ -11,19 +11,34 @@ module.exports = class MessageEvent extends BaseEvent {
   
   async run(client, message) {
     if (message.author.bot) return;
-
+    //console.time('MessageEvent.js');
     
+
     var prefix = "!";
-
-    prefix = await getPrefix(message, prefix);
-
+    //prefix = await getPrefix(message);
+    const guildObject = message.guild;
+    const query = GuildConfig.where({ guildId: guildObject.id });
+    await query.findOne(function (err, guild) {
+      if (err)
+        return handleError(err);
+      if (guild) {
+        prefix = guild.get('prefix');
+      }
+    });
     
+    
+    if(!message.content.startsWith(prefix)) {
+      //console.timeEnd('MessageEvent.js');
+      return 
+    }
+
     if (message.content.startsWith(prefix)) {
+      console.log(`Guild: ${message.guild.name}. ${message.author.username}: ${message.content}`);
       const [cmdName, ...cmdArgs] = message.content
       .slice(prefix.length)
       .trim()
       .split(/\s+/);
-      const command = client.commands.get(cmdName);
+      const command = client.commands.get(cmdName.toLowerCase());
       if (command) {
         message.channel.startTyping();
         if(command.category == "developer" && message.member.id != '542483559500218389'){
@@ -42,17 +57,18 @@ module.exports = class MessageEvent extends BaseEvent {
         message.channel.stopTyping();
       }
     }
+    //console.timeEnd('MessageEvent.js');
   }
 }
 
-async function getPrefix(message, prefix) {
+async function getPrefix(message) {
   const guildObject = message.guild;
   const query = GuildConfig.where({ guildId: guildObject.id });
   await query.findOne(function (err, guild) {
     if (err)
       return handleError(err);
     if (guild) {
-      prefix = guild.get('prefix');
+      return guild.get('prefix');
     }
   });
   return prefix;
