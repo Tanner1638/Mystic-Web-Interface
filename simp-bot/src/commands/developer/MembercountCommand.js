1afcb00bf2c3741d3f7bdc074b115335e4f234d0
Mystic <tanner1638@gmail.com>
2021-02-17 16:29:14 -0600

    Server & Member count (#83)
    Fixes #81
    
    Co-authored-by: bobrienn <bo9br@umsystem.edu>

---
+++ simp-bot/src/commands/developer/MembercountCommand.js
@@ -0,0 +1,23 @@
+const BaseCommand = require('../../utils/structures/BaseCommand');
+
+module.exports = class MembercountCommand extends BaseCommand {
+  constructor() {
+    super('membercount', 'developer', ["mc"]);
+  }
+
+  run(client, message, args) {
+    //message.channel.send('membercount command works');
+    //console.log(client.guilds)
+    var guilds = client.guilds.cache;
+    var serverCount = 0;
+    var memberCount = 0;
+
+    guilds.forEach( guild => {
+      console.log(guild.memberCount);
+      memberCount += guild.memberCount;
+      serverCount += 1;
+    })
+
+    console.log(`Server Count: ${serverCount}\nMember Count: ${memberCount}`);
+  }
+}
--- simp-bot/src/events/ready/ReadyEvent.js
+++ simp-bot/src/events/ready/ReadyEvent.js
@@ -11,8 +11,28 @@ module.exports = class ReadyEvent extends BaseEvent {
     maintenanceMode(client);
     clientCache.set("debug", false);
 
+    //getServingData(client);
+
     
   }
+}
+
+function getServingData(client) {
+  var guilds = client.guilds.cache;
+  var serverCount = 0;
+  var memberCount = 0;
+
+  guilds.forEach(guild => {
+    console.log(guild.memberCount);
+    memberCount += guild.memberCount;
+    serverCount += 1;
+  });
+
+  let streamArg = `Serving ${memberCount} members in ${serverCount} servers!`;
+  client.user.setActivity(streamArg, {
+    type: "STREAMING",
+    url: "https://www.twitch.tv/zvmysticvz"
+  }).catch(console.error);
 }
 
 function maintenanceMode(client) {
