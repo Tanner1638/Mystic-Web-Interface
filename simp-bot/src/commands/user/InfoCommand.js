7669009bfbb05ccfe8a2d34239f931ced9c16689
bobrienn <bo9br@umsystem.edu>
2021-02-17 18:03:10 -0600

    v4.5.4

--- simp-bot/src/commands/user/InfoCommand.js
+++ simp-bot/src/commands/user/InfoCommand.js
@@ -11,7 +11,7 @@ module.exports = class InfoCommand extends BaseCommand {
     const infoEmbed = new Discord.MessageEmbed()
     .setColor("bf3f3f")
     .setTitle("Chaos Bot - Info")
-    .setFooter("Version: v4.5.3 || Last Updated: 2/17/2021");
+    .setFooter("Version: v4.5.4 || Last Updated: 2/17/2021");
 
 
     var guilds = client.guilds.cache;
